157799d44e05241b24a0544462fd85bdd606faf0
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2020-04-20 12:30:35 +1000

    Fix app.js errors

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
 // Constants
 const PORT = 80;
-const path = __dirname + '/views/';
+const path = __dirname + "/views/";
 
 // App
 const app = express();
 const router = express.Router();
 
-router.use(morgan('common'));
+router.use(morgan("common"));
 
 router.use(function (req,res,next) {
   next();
 });
 
-router.get('/', function(req,res){
-  res.sendFile(path + 'index.html');
+router.get("/", function(req,res){
+  res.sendFile(path + "index.html");
 });
 
 
 //app.use(express.static(path));
-app.use('/', router);
+app.use("/", router);
 
 app.listen(PORT, function() {
   console.log(`Simple node web app listening on port ${PORT}`);
